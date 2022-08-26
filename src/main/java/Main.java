40a17b6a92b75905383ba68101668c4161166134
Альберт <sabir116rus@gmail.com>
2022-08-26 18:30:14 +0300

    fixes #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
         long amount = 1000_60;
         boolean registered = true;
 
-        service.calculate(amount, registered);
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+        System.out.println(passed);
     }
 }
 
